ba5c5c36d51b133c72f9fade5aca9f02b8a53136
Hayato.Takasaki <hayato.taka75@gmail.com>
2020-10-24 13:19:36 +1000

    added spyder object and gitignore

--- linkspyder/spyder.py
+++ linkspyder/spyder.py
@@ -4,3 +4,15 @@ spyder.py
 @author: ht-90
 """
 
+
+class Spyder:
+    """A web scraping object for a URL
+
+
+    """
+
+    url = None
+
+    def __init__(self, url):
+        """"""
+        self.url = url
