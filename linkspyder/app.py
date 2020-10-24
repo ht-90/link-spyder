881e6fc5c3d688c33d5cd085fa2e0f79cd782a94
Hayato.Takasaki <hayato.taka75@gmail.com>
2020-10-24 18:27:37 +1000

    WIP:added data router to execute crawling, scraping and visualisation of web links

--- linkspyder/app.py
+++ linkspyder/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from linkspyder.spyder import Spyder
 
 
 app = Flask(__name__)
@@ -7,3 +8,18 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/data", methods=["POST"])
+def data():
+    if request.method == "POST":
+        # Receive user input URL
+        url = request.form["input-url"]
+
+        # Crawl the URL page
+        spyder = Spyder(url=url)
+        spyder.initial_crawl()
+        spyder.deep_crawl()
+        # Create web viz
+        web_viz = spyder.create_web()
+        
+        return render_template("index.html", web_viz=web_viz), 200
