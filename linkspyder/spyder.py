9fff9d684ff4a5e9ab139ce090d1a818b8212543
Hayato.Takasaki <hayato.taka75@gmail.com>
2020-10-24 13:26:35 +1000

    added url web parsing methods

--- linkspyder/spyder.py
+++ linkspyder/spyder.py
@@ -8,11 +8,28 @@ spyder.py
 class Spyder:
     """A web scraping object for a URL
 
+    Attributes:
+        url (str): a starting URL to parse content
+        domain_name (str): a name of domain
+        soup (bs4 object): html content
 
     """
 
     url = None
+    domain_name = None
+    soup = None
 
     def __init__(self, url):
-        """"""
+        """Initialise a web scraper object"""
         self.url = url
+
+    def retrieve_domain(self):
+        """Retrieve a domain name from the URL"""
+        self.domain_name = urlparse(self.url).netloc
+    
+    def parse_page(self):
+        """Parse the page content"""
+        self.soup = BeautifulSoup(
+            requests.get(self.url).content, "html.parser"
+        )
+
