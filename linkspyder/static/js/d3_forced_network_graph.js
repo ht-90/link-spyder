ec77531cd02a5e3385a1635ea8b0aa615974628f
Hayato.Takasaki <hayato.taka75@gmail.com>
2020-11-21 01:02:32 +0000

    fixed drag event listeners

--- linkspyder/static/js/d3_forced_network_graph.js
+++ linkspyder/static/js/d3_forced_network_graph.js
@@ -106,24 +106,22 @@ $("document").ready(function(){
           })
         }
 
+        function dragstarted(event, d) {
+          if (!event.active) simulation.alphaTarget(0.3).restart();
+          d.fx = d.x;
+          d.fy = d.y;
+        }
+
+        function dragged(event, d) {
+          d.fx = event.x;
+          d.fy = event.y;
+        }
+
+        function dragended(event, d) {
+          if (!event.active) simulation.alphaTarget(0);
+          d.fx = null;
+          d.fy = null;
+        }
       });
-
-    function dragstarted(d) {
-      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-      d.fx = d.x;
-      d.fy = d.y;
-    }
-
-    function dragged(d) {
-      d.fx = d3.event.x;
-      d.fy = d3.event.y;
-    }
-
-    function dragended(d) {
-      if (!d3.event.active) simulation.alphaTarget(0);
-      d.fx = null;
-      d.fy = null;
-    }
-
   });
 });
