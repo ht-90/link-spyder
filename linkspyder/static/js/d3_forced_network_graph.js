8c9cc7ce8c805b57c4b5b074583355a13825d6ba
Hayato.Takasaki <hayato.taka75@gmail.com>
2020-11-23 22:19:22 +1000

    added graph legend as svg, css for legend

--- linkspyder/static/js/d3_forced_network_graph.js
+++ linkspyder/static/js/d3_forced_network_graph.js
@@ -100,6 +100,41 @@ $("document").ready(function(){
       node.append("title")
         .text(function(d) { return d.id; });
 
+      // Graph legend
+      var legend = d3.select("div#legend")
+        .append("svg")
+          .attr("width", width)
+          .attr("height", group.length * 25);
+
+      // Add legend symbols
+      legend.selectAll("circle")
+      .data(group)
+      .enter()
+      .append("circle")
+        .attr("cx", 50)
+        .attr("cy", function(d, i){ return 15 + i * 25 })
+        .attr("r", 7)
+        .style("fill", function(d, i){ return color(d.category) })
+
+      // Add legend texts
+      legend.selectAll("labels")
+      .data(group)
+      .enter()
+      .append("text")
+        .attr("x", 70)
+        .attr("y", function(d, i){ return 15 + i * 25 })
+        .style("fill", function(d){ return color(d.category) })
+        .text(function(d){ return d.category_url })
+        .attr("text-anchor", "left")
+        .style("alignment-baseline", "middle")
+
+      legend.append("text")
+          .attr("x", width - 24)
+          .attr("y", 9)
+          .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(function(d){ return d });
+
       // 
       simulation
         .nodes(graph.nodes)
