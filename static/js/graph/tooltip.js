d3d133d05332c1b6880f411a47a0a724256e2c45
Hayato.Takasaki <hayato.taka75@gmail.com>
2021-05-28 19:56:55 +1000

    added tooltip code in separate js file

--- static/js/graph/tooltip.js
+++ static/js/graph/tooltip.js
@@ -8,3 +8,27 @@ function createTooltip(opacity) {
   return tooltip;
 };
 
+
+function addTooltip(obj, tooltip) {
+  obj
+    .on("mouseover", function (event, d) {
+      tooltip.transition().duration(200);
+      tooltip
+        .html(
+          "Page: " +
+          d.page +
+          "<br/>" +
+          "Category: " +
+          d.category +
+          "<br/>" +
+          "Full path: " +
+          d.id
+        )
+        .style("left", event.pageX + "px")
+        .style("top", event.pageY - 28 + "px")
+        .style("opacity", 0.9);
+    })
+    .on("mouseout", function (d) {
+      tooltip.transition().duration(500).style("opacity", 0);
+    });
+};
